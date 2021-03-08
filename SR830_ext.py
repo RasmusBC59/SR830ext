02c684c1455a3ea8d2abeda3ec7bb1b97542cc7b
RasmusBC59 <rbcmail@gmail.com>
2021-03-08 18:08:50 +0100

    now with general set_objekt

--- SR830_ext.py
+++ SR830_ext.py
@@ -1,78 +1,80 @@
 import numpy as np
-import qcodes as qc
-import qcodes.utils.validators as vals
-from qcodes.instrument.base import Parameter
-from qcodes.instrument.parameter import ParameterWithSetpoints
+from qcodes.utils.validators import Numbers, Arrays
+from qcodes.instrument.parameter import ParameterWithSetpoints, Parameter, DelegateParameter
 from qcodes.instrument_drivers.stanford_research.SR830 import SR830 
 
 
 
 
-
-class Buff(ParameterWithSetpoints):
-    def get_raw(self):    
-        axis = self.root_instrument.freq_axis()
-        self.root_instrument.buffer_reset()
-        for x in axis:
-            self.root_instrument.frequency.set(x)
-            self.root_instrument.send_trigger()
-        self.root_instrument.ch1_databuffer.prepare_buffer_readout()    
-        return self.root_instrument.ch1_databuffer.get()
- 
-
-class BuffAxis(Parameter):
+class GeneratedSetPoints(Parameter):
+    """
+    A parameter that generates a setpoint array from start, stop and num points
+    parameters.
+    """
+    def __init__(self, startparam, stopparam, numpointsparam, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._startparam = startparam
+        self._stopparam = stopparam
+        self._numpointsparam = numpointsparam
 
     def get_raw(self):
-        f_start = self.root_instrument.f_start()
-        f_end = self.root_instrument.f_end()
-        f_npts = self.root_instrument.f_npts()        
-        return np.linspace(f_start, f_end, f_npts)
+        return np.linspace(self._startparam(), self._stopparam(),
+                              self._numpointsparam())        
 
 
 class Lockin(SR830):    
         
-
-    def __init__(self, name, address, **kwargs):
+    def __init__(self, name, address, set_object, **kwargs):
         super().__init__(name, address, **kwargs)
-        
-        
+        self.set_object = set_object
+        self.add_parameter('sweep_start',
+                           source=None,
+                           parameter_class=DelegateParameter)
 
+        self.add_parameter('sweep_stop',
+                           source=None,
+                           parameter_class=DelegateParameter)
 
-        self.add_parameter(name='f_npts',
-                           initial_value=500,
-                           label='Number of points',
+        self.add_parameter('sweep_n_points',
+                           unit='',
+                           initial_value=10,
+                           vals=Numbers(1,1e3),
                            get_cmd=None,
-                           set_cmd=None)
-
-        self.add_parameter(name='f_start',
-                           initial_value=1,
-                           label='f_start',
-                           unit='Hz',
-                           get_cmd=None,
-                           set_cmd=None)
+                           set_cmd=None)       
         
-        self.add_parameter(name='f_end',
-                           initial_value=5000,
-                           label='f_end',
-                           unit='Hz',
-                           get_cmd=None,
-                           set_cmd=None)
+        self.add_parameter('setpoints',
+                           parameter_class=GeneratedSetPoints,
+                           startparam=self.sweep_start,
+                           stopparam=self.sweep_stop,
+                           numpointsparam=self.sweep_n_points,
+                           vals=Arrays(shape=(self.sweep_n_points.get_latest,)))
         
-
-        self.add_parameter(name='freq_axis',
-                           label='Frequency',
-                           unit='Hz',
-                           vals=vals.Arrays(shape=(self.f_npts,)),
-                           parameter_class=BuffAxis)
-
         self.add_parameter(name='trace',
+                           get_cmd=self._get_current_data,
                            label='Signal',
                            unit='V',
-                           vals=vals.Arrays(shape=(self.f_npts,)),
-                           setpoints=(self.freq_axis,),
-                           parameter_class=Buff
+                           vals=Arrays(shape=(self.sweep_n_points.get_latest,)),
+                           setpoints=(self.setpoints,),
+                           parameter_class=ParameterWithSetpoints
                            )
         
 
+    def _get_current_data(self):   
+        axis = self.setpoints()
+        self.buffer_reset()
+        for x in axis:
+            self.set_object(x)
+            self.send_trigger()
+        self.prepare_buffer_readout()    
+        return self.ch1_databuffer.get()
 
+    def set_sweep_parameters(self, start_parameter, stop_parameter, label=None):
+        if start_parameter.unit != stop_parameter.unit:
+            raise TypeError("You must sweep from and to "
+                            "parameters with the same unit")
+        self.sweep_start.source = start_parameter
+        self.sweep_stop.source = stop_parameter
+        self.setpoints.unit = start_parameter.unit
+        if label != None:
+            self.setpoints.label = label
 
