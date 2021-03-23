69cf74d2436e092ce5a9132a82d1b8ec5ba2e9a0
RasmusBC59 <rbcmail@gmail.com>
2021-03-23 19:06:23 +0100

    linding

--- SR830_ext.py
+++ SR830_ext.py
@@ -2,55 +2,57 @@ import numpy as np
 from time import sleep
 from qcodes.utils.validators import Numbers, Arrays
 from qcodes.instrument.parameter import ParameterWithSetpoints, Parameter, DelegateParameter
-from qcodes.instrument_drivers.stanford_research.SR830 import SR830 
+from qcodes.instrument_drivers.stanford_research.SR830 import SR830
 
 
+class SR830_ext(SR830):
+    """
+    This  is an extesension  of the Stanford Research Systems SR830
+    Lock-in Amplifier driver
 
-
-     
-class SR830_ext(SR830):    
-        
+    The extension adds the functionallety to sweep a DelegateParameter
+    from an other instruments while writiing to the buffer by sending software triggers.
+    Furthermore, it adds the prepare_and_get_buffer function which is needed by bundle_lockin.py
+    """
     def __init__(self, name, address, **kwargs):
         super().__init__(name, address, **kwargs)
-
 
         self.add_parameter('sweep_param',
                            source=None,
                            parameter_class=DelegateParameter)
 
-        
         self.add_parameter('sweep_start',
                            unit='',
                            initial_value=0,
                            get_cmd=None,
-                           set_cmd=None) 
+                           set_cmd=None)
 
         self.add_parameter('sweep_stop',
                            unit='',
                            initial_value=1,
                            get_cmd=None,
-                           set_cmd=None)                                                 
+                           set_cmd=None)                                            
 
         self.add_parameter('sweep_n_points',
                            unit='',
                            initial_value=10,
-                           vals=Numbers(1,1e3),
+                           vals=Numbers(1, 1e3),
                            get_cmd=None,
-                           set_cmd=None)       
-        
+                           set_cmd=None)
+
         self.add_parameter('setpoints',
                            parameter_class=GeneratedSetPoints,
                            startparam=self.sweep_start,
                            stopparam=self.sweep_stop,
                            numpointsparam=self.sweep_n_points,
                            vals=Arrays(shape=(self.sweep_n_points.get_latest,)))
-                
+
         self.add_parameter('sweep_wait_time',
                            unit='s',
                            initial_value=0.1,
                            get_cmd=None,
-                           set_cmd=None) 
-        
+                           set_cmd=None)
+
         self.add_parameter(name='trace',
                            get_cmd=self._get_current_data,
                            label='Signal',
@@ -59,36 +61,35 @@ class SR830_ext(SR830):
                            setpoints=(self.setpoints,),
                            parameter_class=ParameterWithSetpoints
                            )
-        
 
-    def _get_current_data(self):   
+    def _get_current_data(self):
         axis = self.setpoints()
         self.buffer_reset()
         for x in axis:
             self.sweep_param.set(x)
             sleep(self.sweep_wait_time.get())
             self.send_trigger()
-        self.ch1_databuffer.prepare_buffer_readout()    
+        self.ch1_databuffer.prepare_buffer_readout()
         return self.ch1_databuffer.get()
 
-    def prepare_and_get_buffer(self):   
-        self.ch1_databuffer.prepare_buffer_readout()    
+    def prepare_and_get_buffer(self):
+        self.ch1_databuffer.prepare_buffer_readout()
         return self.ch1_databuffer.get()
 
     def set_sweep_parameters(self,sweep_param, start, stop, n_points=10, wait_time=0.1, label=None):
         self.sweep_param.source = sweep_param
-        
+
         self.sweep_start.unit = sweep_param.unit
         self.sweep_start.vals = sweep_param.vals
         self.sweep_start.set(start)
-        
+
         self.sweep_stop.unit = sweep_param.unit
         self.sweep_stop.vals = sweep_param.vals
         self.sweep_stop.set(stop)
         self.sweep_n_points.set(n_points)
         self.sweep_wait_time.set(wait_time)
         self.setpoints.unit = sweep_param.unit
-        if label != None:
+        if label is not None:
             self.setpoints.label = label
 
 
@@ -103,8 +104,6 @@ class GeneratedSetPoints(Parameter):
         self._stopparam = stopparam
         self._numpointsparam = numpointsparam
 
-
     def get_raw(self):
         return np.linspace(self._startparam(), self._stopparam(),
-                              self._numpointsparam())   
-
+                           self._numpointsparam())
