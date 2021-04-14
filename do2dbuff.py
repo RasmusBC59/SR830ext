1ab5a50ebaab6c55622b7707afc8f38f0be5fd50
RasmusBC59 <rbcmail@gmail.com>
2021-04-14 10:53:42 +0200

    add ExitStack and Try and error block for faid buffer get

--- do2dbuff.py
+++ do2dbuff.py
@@ -6,11 +6,12 @@ from tqdm import tqdm_notebook as tqdm
 from qcodes.instrument.parameter import _BaseParameter
 from qcodes import Measurement
 from typing import List
+from contextlib import ExitStack
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-file_handler = logging.FileHandler('bundletimeing.log')
+file_handler = logging.FileHandler('do2dbuff.log')
 file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 
@@ -23,7 +24,9 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
                write_period: float = 1.,
                threading: List[bool] = [True, True, True, True],
                label: str = None,
-               channels = 0
+               channels = 0,
+               attempts_to_get:int = 3,
+               delay_fast_increase:float = 0.0
                ):
     """
     This is a do2d to be used for buffered instruments.
@@ -50,7 +53,7 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
     logger.info('Starting do2d_multi with {}'.format(num_points_slow * num_points_fast))
     logger.info('write_in_background {},threading buffer_reset {},threading send_trigger {},threading  get trace {}'.format(*threading))
     begin_time = time.perf_counter()
-    meas = Measurement()
+    
     
 
 
@@ -63,6 +66,7 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
     interval_slow.set_description("Slow parameter")
     set_points_fast = lockins[0].sweep_setpoints
 
+    meas = Measurement()
     meas.write_period = write_period
     meas.register_parameter(set_points_fast)
     meas.register_parameter(param_fast)
@@ -88,6 +92,7 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
         meas_no_buffer.register_parameter(param_slow)
         for device in devices_no_buffer:
             meas_no_buffer.register_parameter(device, setpoints=(param_slow, param_fast))
+        
 
 
     time_fast_loop = 0.0
@@ -95,11 +100,17 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
     time_buffer_reset = 0.0
     time_trigger_send = 0.0
     time_get_trace = 0.0
+    #my_meas (meas.run(write_in_background=threading[0]) as datasaver, meas_no_buffer.run(write_in_background=threading[0])  as datasaver_no_buffer
+    cm_datasaver = meas.run(write_in_background=threading[0])
+    if devices_no_buffer is not None: 
+        cm_datasaver_no_buffer = meas_no_buffer.run(write_in_background=threading[0])
 
-
-    with meas.run(write_in_background=threading[0]) as datasaver, meas_no_buffer.run(write_in_background=threading[0])  as datasaver_no_buffer:
-
-
+    with ExitStack() as cmx: #cm1 as datasaver, cm2 as datasaver_no_buffer:
+        cmx.enter_context(cm_datasaver)
+        datasaver = cm_datasaver.datasaver
+        if devices_no_buffer is not None:  
+            cmx.enter_context(cm_datasaver_no_buffer)
+            datasaver_no_buffer = cm_datasaver_no_buffer.datasaver
         for point_slow in interval_slow:
             param_slow.set(point_slow)
             data = []
@@ -108,60 +119,73 @@ def do2d_multi(param_slow: _BaseParameter, start_slow: float, stop_slow: float,
             if devices_no_buffer is not None:
                 data_no_buffer = []
                 data_no_buffer.append((param_slow, param_slow.get()))
-
-            begin_time_temp_buffer = time.perf_counter()
-            if threading[1]:
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    for lockin in lockins:
-                        executor.submit(lockin.buffer_reset)
-            else:
-                for lockin in lockins:
-                    lockin.buffer_reset()
-            time_buffer_reset += time.perf_counter() - begin_time_temp_buffer
-
-            begin_time_temp_fast_loop = time.perf_counter()
-            interval_fast = tqdm(set_points_fast.get(), position=1, leave=False)
-            interval_fast.set_description("Fast parameter")
-            for point_fast in interval_fast:
-                begin_time_temp_set_fast = time.perf_counter()
-                param_fast.set(point_fast)
-
-                time_set_fast += time.perf_counter() - begin_time_temp_set_fast
-                begin_time_temp_trigger = time.perf_counter()
-                if threading[2]:
-                    with concurrent.futures.ThreadPoolExecutor() as executor:
+            attempts = 0
+            while attempts < attempts_to_get:
+                try:  
+                    begin_time_temp_buffer = time.perf_counter()
+                    if threading[1]:
+                        with concurrent.futures.ThreadPoolExecutor() as executor:
+                            for lockin in lockins:
+                                executor.submit(lockin.buffer_reset)
+                    else:
                         for lockin in lockins:
-                            executor.submit(lockin.send_trigger)
-                else:
-                    for lockin in lockins:
-                        lockin.send_trigger()
+                            lockin.buffer_reset()
+                    time_buffer_reset += time.perf_counter() - begin_time_temp_buffer
 
-                time_trigger_send += time.perf_counter() - begin_time_temp_trigger
-                
-                if devices_no_buffer is not None:
-                    fast = param_fast.get()
-                    data_no_buffer.append((param_fast, fast))
-                    for device in devices_no_buffer: 
-                        device_value = device.get()
-                        data_no_buffer.append((device, device_value))
-                    datasaver_no_buffer.add_result(*data_no_buffer)
+                    begin_time_temp_fast_loop = time.perf_counter()
+                    interval_fast = tqdm(set_points_fast.get(), position=1, leave=False)
+                    interval_fast.set_description("Fast parameter")
+                    for point_fast in interval_fast:
+                        begin_time_temp_set_fast = time.perf_counter()
+                        param_fast.set(point_fast)
 
-            time_fast_loop += time.perf_counter() - begin_time_temp_fast_loop
+                        time_set_fast += time.perf_counter() - begin_time_temp_set_fast
+                        begin_time_temp_trigger = time.perf_counter()
+                        if threading[2]:
+                            with concurrent.futures.ThreadPoolExecutor() as executor:
+                                for lockin in lockins:
+                                    executor.submit(lockin.send_trigger)
+                        else:
+                            for lockin in lockins:
+                                lockin.send_trigger()
 
-            begin_time_temp_trace = time.perf_counter()
-            if threading[3]:
-                with concurrent.futures.ThreadPoolExecutor() as executor:
-                    data_trace = executor.map(trace_tuble, traces)
+                        time_trigger_send += time.perf_counter() - begin_time_temp_trigger
+                        
+                        if devices_no_buffer is not None:
+                            fast = param_fast.get()
+                            data_no_buffer.append((param_fast, fast))
+                            for device in devices_no_buffer: 
+                                device_value = device.get()
+                                data_no_buffer.append((device, device_value))
+                            datasaver_no_buffer.add_result(*data_no_buffer)
 
-                data += list(data_trace)
-            else:
-                #data = []
-                for trace in traces:
-                    data.append((trace, trace.get()))
-            time_get_trace += time.perf_counter() - begin_time_temp_trace
+                    time_fast_loop += time.perf_counter() - begin_time_temp_fast_loop
 
-            #data.append((param_slow, param_slow.get()))
-            data.append((set_points_fast, set_points_fast.get()))
+                    begin_time_temp_trace = time.perf_counter()
+                    if threading[3]:
+                        with concurrent.futures.ThreadPoolExecutor() as executor:
+                            data_trace = executor.map(trace_tuble, traces)
+
+                        data += list(data_trace)
+                    else:
+                        #data = []
+                        for trace in traces:
+                            data.append((trace, trace.get()))
+                    time_get_trace += time.perf_counter() - begin_time_temp_trace
+
+                    #data.append((param_slow, param_slow.get()))
+                    data.append((set_points_fast, set_points_fast.get()))
+                    break
+                except Exception as e:
+                    print(e)
+                    attempts += 1
+                    delay_fast += delay_fast_increase
+                    print(attempts)
+                    print(delay_fast)
+                    if attempts < attempts_to_get:
+                        print('getting the buffer failed, will try again')
+                    else:
+                        print('getting the buffer failed, will go to next slow_point')
             datasaver.add_result(*data)
 
     message = 'Have finished the measurement in {} seconds'.format(time.perf_counter()-begin_time)
